3f51e19b459ee092c8e90b26dc26d774ebc1aea1
NicoloFaccio <nicolo.faccio1@gmail.com>
2024-01-11 18:07:46 +0100

    working on the function of the addEventListener

--- Blocco 1 - snake 6/assets/JS/main.js
+++ Blocco 1 - snake 6/assets/JS/main.js
@@ -1,2 +1,14 @@
 //Blocco 1 - snake 6 -Stampa il cubo dei primi N numeri, dove N è un numero indicato dall’utente.
 
+//INITIALISATION OF THE VARIABLES
+const numberRandomHtml = document.getElementById("numberRandom")
+const buttonSubmit = document.getElementById("btn-submit")
+const resultHmtl = document.getElementById("result")
+
+//CREATION OF THE FUNCTIONS 
+
+buttonSubmit.addEventListener("click", function(){
+    console.log(numberRandomHtml.value)
+    resultHmtl.innerHTML = Math.pow(numberRandomHtml, 2);
+})
+

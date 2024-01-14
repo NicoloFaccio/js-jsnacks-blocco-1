38cc5d01c919c41d079001a0aebe0825176aa9e7
NicoloFaccio <nicolo.faccio1@gmail.com>
2024-01-14 20:45:20 +0100

    created the cycle for and the document write.ln

--- Blocco 2 - snake 3/assets/JS/main.js
+++ Blocco 2 - snake 3/assets/JS/main.js
@@ -1,1 +1,14 @@
 //Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
+
+const numbers = [12, 27, 33, 42, 55, 61];
+
+let sum = 0
+
+for (let i = 0; i < numbers.length; i++){
+    if (i % 2 !== 0){
+        console.log(sum += numbers[i])
+    }
+}
+
+document.writeln(`La somma dei numeri in posizione dispari è: 
+${sum}`)

6ac9fdfa3e27b07791c4b23ba5336bfeee3d9dd7
NicoloFaccio <nicolo.faccio1@gmail.com>
2024-01-14 17:52:02 +0100

    Eliminated the form and inserted the prompt with the cycle for

--- Blocco 1 - snake 6/assets/JS/main.js
+++ Blocco 1 - snake 6/assets/JS/main.js
@@ -1,14 +1,13 @@
 //Blocco 1 - snake 6 -Stampa il cubo dei primi N numeri, dove N è un numero indicato dall’utente.
 
 //INITIALISATION OF THE VARIABLES
-const numberRandomHtml = document.getElementById("numberRandom")
-const buttonSubmit = document.getElementById("btn-submit")
-const resultHmtl = document.getElementById("result")
+const numberRandomHtml = parseInt(prompt("Inserire un numero; ", 4)); 
+const resultHmtl = document.getElementById("result");
 
 //CREATION OF THE FUNCTIONS 
-
-buttonSubmit.addEventListener("click", function(){
-    console.log(numberRandomHtml.value)
-    resultHmtl.innerHTML = Math.pow(numberRandomHtml, 2);
-})
+for (let i = 0; i < numberRandomHtml; i++) {
+    let cube = Math.pow(i , 3)
+    console.log(cube)
+    resultHmtl.innerHTML = (`Ecco il cubo del numero inserito; ${cube}`)
+}
 

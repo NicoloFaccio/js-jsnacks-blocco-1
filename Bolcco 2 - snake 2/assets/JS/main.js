9f262a263cdbe0c95e130d4e168af51ef1363560
NicoloFaccio <nicolo.faccio1@gmail.com>
2024-01-14 18:17:52 +0100

    created the cycle for

--- Bolcco 2 - snake 2/assets/JS/main.js
+++ Bolcco 2 - snake 2/assets/JS/main.js
@@ -1,5 +1,11 @@
 //Generatore di “nomi cognomi” casuali: il Grande Gatsby ha una lista di nomi e una lista di cognomi, e da queste vuole generare una falsa lista di invitati con nome e cognome.
 
-const name = ["Luca", "Paolo", "Marco", "Laura", "Veronica"];
+const nameHtml = ["Luca", "Paolo", "Marco", "Laura", "Veronica"];
 const surname = ["Verdi", "Rossi", "Gialli", "Blue", "Violet"];
-const mix = [""]; 
+const mix = [];
+
+for (let i = 0; i < nameHtml.length; i++){
+mix.push( nameHtml[i] + " " + surname[i] )
+}
+
+console.log(mix)
